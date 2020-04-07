5eeb7355226d49a1d46e13575e12bdd1e1aee20a
LinuxRocks2000 <you@example.com>
2020-04-07 09:56:56 -0400

    Add support for my pip repo

--- webserver/main.py
+++ webserver/main.py
@@ -3,55 +3,7 @@ import json
 import mimetypes
 import os
 import controls
-class ServerListenable:
-    def __init__(self,host,port):
-        self.sckt=socket.socket()
-        self.sckt.bind((host,port))
-    def run(self):
-        self.sckt.listen(5)
-        while True:
-            self.connection, (client_host, client_port) = self.sckt.accept()
-            recieved=self.connection.recv(1024)
-            try:
-                ppt=self.parseRequest(recieved)
-                if ppt["reqtype"]=="POST":
-                    self.handle_post(ppt,self.connection)
-                elif ppt["reqtype"]=="GET":
-                    self.handle_get(ppt,self.connection)
-                else:
-                    raise Exception
-            except Exception as e:
-                self.handle_aux(recieved,self.connection)
-                print(e)
-            self.connection.close()
-    def handle_post(self,data,connection):
-        pass
-    def handle_get(self,data,connection):
-        pass
-    def handle_aux(self,req,connection):
-        pass
-    def parseRequest(self,data):
-        copy=data.decode()
-        copy=copy.replace("\r","")
-        copy=copy.split("\n")
-        toreturn={}
-        if copy[0][0:4]=="GET ":
-            toreturn["reqtype"]="GET"
-        if copy[0][0:4]=="POST":
-            toreturn["reqtype"]="POST"
-        toreturn["reqlocation"]=copy[0].split(" ")[1]
-        toreturn["useragent"]=copy[2].split(" ")[1]
-        toreturn["mimetype"]=(copy[3][8:]).split(",")
-        toreturn["langtype"]=(copy[4][17:]).split(",")
-        if toreturn["reqtype"]=="POST":
-            toreturn["data"]=json.loads(copy[13])
-        return toreturn
-    def send_file_headers(self, connection, request_file=None,contenttype=None):
-        connection.send("HTTP/1.0 200 OK\n".encode())
-        bork = "Content-Type: " + (contenttype or mimetypes.guess_type(request_file)[0]) + "\n"
-        connection.send(bork.encode())
-        connection.send("\n".encode())
-
+from serverutils import ServerListenable
 
 class Server(ServerListenable):
     def handle_post(self,data,connection):
@@ -78,7 +30,8 @@ class Server(ServerListenable):
             self.send_file(data,connection)
     def handle_post(self,data,connection):
         if data["reqlocation"][0:9]=="/CONTROLS":
-            
+            controls.trigger_top(self,connection,data)
+            connection.close()
     def handle_aux(self,req,connection):
         pass
 
